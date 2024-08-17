295294ab49eb36755aace2292c8e9765f86cec1a
rksorry7255tu <rk7255053708@gmail.com>
2024-08-17 12:12:36 +0530

    adding appapp.js file

---
+++ appapp.js
@@ -0,0 +1,100 @@
+let boxes=document.querySelectorAll(".box");
+let resetbutt=document.querySelector("#reset");
+let newGameBtn=document.querySelector("#new-btn");
+let msgContainer=document.querySelector(".msg-container");
+let msg=document.querySelector("#msg");
+
+let turnO=true;
+const winPatterns=[
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8]
+];
+let count=0;
+boxes.forEach((box) =>{
+    
+    box.addEventListener("click",()=>{
+      
+        if(turnO===true){
+            box.innerText="X";
+            turnO=false;
+
+        }else{
+            box.innerText="O";
+            
+            turnO=true;
+        }
+        count++;
+        box.disabled=true;
+        let iswinner=checkWinner();
+         if(count===9&&!iswinner){
+           
+             showDraw();
+         }
+ 
+    });
+    
+   
+});
+
+
+
+const enableBoxes =()=>{
+    for(let box of boxes){
+        box.disabled=false;
+        box.innerText="";
+    }
+}
+
+const disableBoxes =()=>{
+    for(let box of boxes){
+        box.disabled=true;
+    }
+}
+const showDraw=()=>{
+    msg.innerText=`Game Draw`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+}
+
+const showWinner=(winner)=>{
+    
+    msg.innerText=`Congratulations,Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+}
+
+const checkWinner=()=>{
+    for(let pattern of winPatterns){
+       
+        let pos1Val=boxes[pattern[0]].innerText;
+        let pos2Val= boxes[pattern[1]].innerText;
+        let pos3Val=boxes[pattern[2]].innerText;
+
+
+        if(pos1Val !=""&&pos2Val !=""&&pos3Val !=""){
+            if(pos1Val===pos2Val && pos2Val===pos3Val){  
+                showWinner(pos1Val);
+                
+            }
+            
+            
+        }  
+    }
+};
+
+const resetGame=()=>{
+    turnO=true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+    count=0;
+}
+
+
+newGameBtn.addEventListener("click",resetGame);
+resetbutt.addEventListener("click",resetGame);
